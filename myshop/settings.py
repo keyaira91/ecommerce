dd44a37bee868b69ae74a8df7b77588042d2e7ce
keyaira91 <keyaira22@hotmail.com>
2021-11-08 12:47:28 -0500

    updating settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
+
+import django_heroku
+django_heroku.settings(locals())
